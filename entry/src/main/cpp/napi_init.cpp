9600315e997590095bd45cfd1ea7d837f0628bfa
ZX_RAY <108106842+ZX-RAY1124@users.noreply.github.com>
2026-08-23 17:15:35 +0800

    The dependency has been fixed. Acked-by: ZX_RAYER <zx_ray1124@163.com>

--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -1,35 +1,60 @@
 #include "napi/native_api.h"
-#include "essentia/essentia.h"
-
+#include "hilog/log.h"
 // 暂时不引用 essentia，先验证编译链路没问题
 // #include "essentia/essentia.h"
 
 static napi_value Add(napi_env env, napi_callback_info info)
 {
-    size_t argc = 2;
-    napi_value args[2] = {nullptr};
+    size_t argc = 2;       //收集两个参数
+    napi_value args[2] = {nullptr};       // 用一个数组接住传进来的参数
     napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
-    double value0, value1;
-    napi_get_value_double(env, args[0], &value0);
-    napi_get_value_double(env, args[1], &value1);
+    double value0, value1;                //定义两个变量
+    napi_get_value_double(env, args[0], &value0);     //args[0] 第一个参数 value0
+    napi_get_value_double(env, args[1], &value1);     //args[1] 第二个参数 value1
 
     napi_value sum;
-    napi_create_double(env, value0 + value1, &sum);
+    napi_create_double(env, value0 + value1, &sum);       //计算结果并且将返回值丢给 sum   
     return sum;
 }
 
+static napi_value Squire(napi_env env, napi_callback_info info){
+    size_t argc = 2;
+    napi_value args[2] = {nullptr};
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    double num = 0.0;
+    int32_t squire = 0;
+    napi_get_value_double(env, args[0], &num);
+    napi_get_value_int32(env, args[1], &squire);
+
+    // 计算 num 的 squire 次幂
+    double result = 1.0;
+    for (int32_t i = 0; i < squire; i++) {
+        result *= num;
+    }
+
+    napi_value final;
+    napi_create_double(env, result, &final);
+    return final;
+}
+
+
+//注册函数，告诉系统有一个叫add的函数
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports)
 {
+    OH_LOG_INFO(LOG_APP, "Init called!");
     napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr }
+                // { "add" 是 ArkTS 侧调用时用的名字, Add 是上面的 C++ 函数 }
+        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr },
+        {"squire", nullptr, Squire, nullptr,nullptr,nullptr, napi_default, nullptr}
     };
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
 EXTERN_C_END
 
+//定义模块
 static napi_module demoModule = {
     .nm_version = 1,
     .nm_flags = 0,
@@ -42,5 +67,6 @@ static napi_module demoModule = {
 
 extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
 {
+    OH_LOG_INFO(LOG_APP, "RegisterEntryModule called!");
     napi_module_register(&demoModule);
 }
