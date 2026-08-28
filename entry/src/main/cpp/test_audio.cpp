045cf2c2d3cd2f13a5f17d3008919984a8771ec3
ZX_RAY <108106842+ZX-RAY1124@users.noreply.github.com>
2026-08-28 17:19:43 +0800

    add Player

--- entry/src/main/cpp/test_audio.cpp
+++ entry/src/main/cpp/test_audio.cpp
@@ -5,8 +5,14 @@
 // please include "napi/native_api.h".
 
 #include "test_audio.h"
+#include "LiveStretchPlayer.h"
+#include "hilog/log.h"
 
-test_audio::test_audio() : name("dev") {}
+
+
+test_audio::test_audio():player(2, 44100, 65536, 1.0, 512) {
+   
+}
 
 void test_audio::make_name(std::string name) {
     this->name = name;
@@ -15,3 +21,20 @@ void test_audio::make_name(std::string name) {
 std::string test_audio::get_name() {
     return this->name;
 }
+
+void test_audio::init_audio(char file_path[]){
+    file = std::fopen(file_path, "r");
+    if (file == nullptr){
+        OH_LOG_ERROR(LOG_APP, "Open File Error!");
+        return;
+    }
+}
+
+void test_audio::audio_play() {
+   
+}
+
+void test_audio::audio_pause() {
+    
+}
+
--- entry/src/main/cpp/test_audio.h
+++ entry/src/main/cpp/test_audio.h
@@ -8,14 +8,20 @@
 #define BEAT_PER_MILE_TEST_AUDIO_H
 
 #include <string>
+#include "LiveStretchPlayer.h"
+
 struct test_audio {
 private:
     std::string name;
-    
+    LiveStretchPlayer player;
+    FILE *file;
 public:
     test_audio();
     void make_name(std::string name);
     std::string get_name();
+    void init_audio(char file_path[]);
+    void audio_play();
+    void audio_pause();
 };
 
 #endif //BEAT_PER_MILE_TEST_AUDIO_H
